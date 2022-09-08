f16b27c48b45f0a96a761031c75779a63b70a6ff
Alex <Alex_Etzel1@baylor.edu>
2022-09-08 12:46:26 -0500

    start

--- avg_steps.py
+++ avg_steps.py
@@ -6,9 +6,8 @@ outfile=open('avg_steps.csv','w')
 
 csvfile=csv.reader(infile, delimiter=',')
 
-yo=0
 i=0
-balls=0
+a=0
 
 next(csvfile)
 
@@ -18,17 +17,19 @@ month=1
 
 outfile.write('Month, Average Steps\n')
 for x in csvfile:
-    balls+=float(x[1])
+    a+=float(x[1])
     i+=1
     if float(x[0])!=month:
         month-=1
-        avg=(balls/i)
+        avg=(a/i)
         avg=format(avg,',.2f')
         print(prof_b[month]+', '+str(avg))
         outfile.write(prof_b[month]+', '+ str(avg)+'\n')
         month+=2
+        i=0
+        a=0
     if float(x[0])==month:
-        balls+=float(x[1])
+        a+=float(x[1])
         i+=1
     
 outfile.close()
